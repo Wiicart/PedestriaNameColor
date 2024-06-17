ed77ce7b11f9c6285422408bebf60b1710fc6e89
wiicart <sunglassesaj@gmail.com>
2024-06-17 13:42:03 -0500

    Update JoinListener.java
    remove if statements

--- src/main/java/com/pedestriamc/namecolor/JoinListener.java
+++ src/main/java/com/pedestriamc/namecolor/JoinListener.java
@@ -9,13 +9,22 @@ public class JoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event){
         StoredPlayer player = StoredPlayers.loadStoredPlayer(event.getPlayer());
         if(player != null){
-            if(player.getType() == StoredPlayer.Type.CHAT_COLOR){
-                SetNameColor.setColor(event.getPlayer(), player.getChatColor(), false);
-            }else if(player.getType() == StoredPlayer.Type.RGB_COLOR){
-                SetNameColor.setColor(event.getPlayer(), player.getColor(), false);
-            }else if(player.getType() == StoredPlayer.Type.NICKNAME){
-                SetNickname.setNick(player.getNickname(), event.getPlayer(), false);
+            // Determines the way the name is set, and uses the proper method to set displayName
+            switch (player.getType()){
+                case NICKNAME:
+                    SetName.setNick(player.getNickname(), event.getPlayer(), false);
+                    break;
+                case RGB_COLOR:
+                    SetName.setColor(event.getPlayer(), player.getColor(), false);
+                    break;
+                case CHAT_COLOR:
+                    SetName.setColor(event.getPlayer(), player.getChatColor(), false);
+                    break;
             }
+        }else{
+            //Update display name to default color code from config
+            SetName.setColor(event.getPlayer(), NameColor.getInstance().getDefaultColor(), false);
         }
     }
 }
+
