e8bd2fc388ecbb946ff7731e500468d2c235f1f9
wiicart <sunglassesaj@gmail.com>
2024-06-17 16:29:25 -0500

    Update NameColor.java

--- src/main/java/com/pedestriamc/namecolor/NameColor.java
+++ src/main/java/com/pedestriamc/namecolor/NameColor.java
@@ -1,109 +1,178 @@
-package com.pedestriamc.namecolor.commands;
+package com.pedestriamc.namecolor;
 
-import com.pedestriamc.namecolor.NameColor;
-import com.pedestriamc.namecolor.SetName;
-import net.md_5.bungee.api.ChatColor;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
+import com.pedestriamc.namecolor.commands.NameColorCommand;
+import com.pedestriamc.namecolor.commands.NicknameCommand;
+import com.pedestriamc.namecolor.commands.WhoIsCommand;
+import com.pedestriamc.namecolor.listeners.JoinListener;
+import com.pedestriamc.namecolor.listeners.LeaveListener;
+import com.pedestriamc.namecolor.tabcompleters.NameColorCommandTabCompleter;
+import com.pedestriamc.namecolor.tabcompleters.NicknameTabCompleter;
+import com.pedestriamc.namecolor.tabcompleters.WhoIsTabCompleter;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 
-public class NameColorCommand implements CommandExecutor {
+public final class NameColor extends JavaPlugin {
 
-    private final String[] colors = new String[]{"BLACK", "DARKBLUE", "DARKGREEN", "DARKAQUA", "DARKRED", "DARKPURPLE", "GOLD", "GRAY", "DARKGRAY", "BLUE", "GREEN", "AQUA", "RED", "LIGHTPURPLE", "YELLOW", "WHITE"};
-    private final ChatColor[] chatColors = new ChatColor[]{ChatColor.BLACK, ChatColor.DARK_BLUE, ChatColor.DARK_GREEN, ChatColor.DARK_AQUA, ChatColor.DARK_RED, ChatColor.DARK_PURPLE, ChatColor.GOLD, ChatColor.GRAY, ChatColor.DARK_GRAY, ChatColor.BLUE, ChatColor.GREEN, ChatColor.AQUA, ChatColor.RED, ChatColor.LIGHT_PURPLE, ChatColor.YELLOW, ChatColor.WHITE};
-    private final FileConfiguration config = NameColor.getInstance().getConfigFile();
+    private FileConfiguration config = getConfig();
+    private Essentials essentials;
+    private static NameColor instance;
+    private File playersFile;
+    private FileConfiguration playersConfig;
+    private String mode;
+    private boolean notify;
+    private String defaultColor;
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player selectedPlayer;
-        StringBuilder color = new StringBuilder();
-        //Check if command is invalid or player does not have permission
-        if(sender instanceof Player){
-            //Default selectedPlayer is sender
-            selectedPlayer = (Player) sender;
-            //Check for permissions
-            if(!sender.hasPermission("namecolor.set") && !sender.hasPermission("namecolor.*")){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("no-perms")));
-                return true;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+        saveDefaultConfig();
+        setupPlayersFile();
+        if(config.getString("default-color") != null){
+            defaultColor = config.getString("default-color");
+        }else {
+            defaultColor = "&f";
+        }
+        this.getModeFromConfig();
+        SetName.initialize();
+        int pluginId = 22112;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new SimplePie("mode", () -> {
+            return getMode();
+        }));
+        this.getCommand("namecolor").setExecutor(new NameColorCommand());
+        this.getCommand("nick").setExecutor(new NicknameCommand());
+        this.getCommand("nickname").setExecutor(new NicknameCommand());
+        this.getCommand("whois").setExecutor(new WhoIsCommand());
+        this.getCommand("namecolor").setTabCompleter(new NameColorCommandTabCompleter());
+        this.getCommand("nick").setTabCompleter(new NicknameTabCompleter());
+        this.getCommand("nickname").setTabCompleter(new NicknameTabCompleter());
+        this.getCommand("whois").setTabCompleter(new WhoIsTabCompleter());
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new LeaveListener(), this);
+        Bukkit.getLogger().info("[NameColor] Enabled");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        this.savePlayersConfig();
+        Bukkit.getLogger().info("NameColor disabled");
+    }
+    public static NameColor getInstance(){
+        return instance;
+    }
+    public FileConfiguration getConfigFile(){
+        return config;
+    }
+    public String getPrefix(){
+        return config.getString("prefix");
+    }
+    public String getMode(){
+        return mode;
+    }
+    private void setupPlayersFile() {
+        playersFile = new File(getDataFolder(), "players.yml");
+        if (!playersFile.exists()) {
+            playersFile.getParentFile().mkdirs();
+            try {
+                playersFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            //Check if args == 0
-            if(args.length == 0){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("insufficient-args")));
-                return true;
-            }
-            if (args[0].equalsIgnoreCase("HELP")) {
-                for(String msgs : NameColor.getInstance().getConfigFile().getStringList("namecolor-help")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',msgs));
-                }
-                return true;
-            }
-            //Check if command is setting a different player's nickname
-            if(args.length >= 2 && Bukkit.getPlayer(args[args.length - 1]) != null){
-                if (!sender.hasPermission("namecolor.set.others") && !sender.hasPermission("namecolor.*")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("no-perms")));
-                    return true;
-                }else{
-                    selectedPlayer = Bukkit.getPlayer(args[args.length - 1]);
-                }
-            }
-        }else{ //Sender is server
-            if(args.length < 2){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("insufficient-args")));
-                return true;
-            }
-            if(Bukkit.getPlayer(args[args.length - 1]) != null){
-                selectedPlayer = Bukkit.getPlayer(args[args.length - 1]);
+        }
+        playersConfig = YamlConfiguration.loadConfiguration(playersFile);
+    }
+
+    public FileConfiguration getPlayersConfig() {
+        return playersConfig;
+    }
+    public void savePlayersConfig() {
+        try {
+            playersConfig.save(playersFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void getModeFromConfig(){ //get mode from config file, consider if option set in config is viable
+        if(config.getString("mode").equalsIgnoreCase("auto")){
+            if(getServer().getPluginManager().getPlugin("Essentials") != null){
+                Bukkit.getLogger().info("[NameColor] Using Essentials mode");
+                mode = "essentials";
+                essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
             }else{
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-player")));
-                return true;
+                Bukkit.getLogger().info("[NameColor] Essentials plugin not found, using Server mode");
+                mode = "server";
             }
-        }
-        //Checks done, time to process command
-        //Determine type of color code, and begin building string
-        if(Arrays.asList(colors).contains(args[0].toUpperCase())){ //https://stackoverflow.com/questions/1128723/how-do-i-determine-whether-an-array-contains-a-particular-value-in-java
-            color.append(chatColors[Arrays.asList(colors).indexOf(args[0].toUpperCase())]);
-        }else if(args[0].toUpperCase().matches(("^#[a-fA-F0-9]{6}$"))){
-            color.append("&");
-            color.append(args[0].toUpperCase());
+        }else if(config.getString("mode").equalsIgnoreCase("essentials")){
+            if(getServer().getPluginManager().getPlugin("Essentials") != null){
+                Bukkit.getLogger().info("[NameColor] Using Essentials mode");
+                mode = "essentials";
+                essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+            }else{
+                Bukkit.getLogger().info("[NameColor] Essentials plugin not found, defaulting to Server mode");
+                mode = "server";
+            }
+        }else if(config.getString("mode").equalsIgnoreCase("server")){
+            Bukkit.getLogger().info("[NameColor] Using Server mode");
+            mode = "server";
         }else{
-            //Invalid color as first arg
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-color")));
+            Bukkit.getLogger().info("[NameColor] Invalid mode in config.yml, defaulting to Server mode");
+            mode = "server";
         }
-        //process additional options such as bold, italics, etc.
-        for(int i = 1; i<args.length; i++){
-            switch(args[i].toUpperCase()){
-                case "BOLD":
-                    color.append("&l");
-                    break;
-                case "UNDERLINE":
-                    color.append("&n");
-                    break;
-                case "ITALICS":
-                    color.append("&o");
-                    break;
-                case "MAGIC":
-                    color.append("&k");
-                    break;
-                case "STRIKE":
-                    color.append("&m");
-                    break;
-                case "ITALIC":
-                    color.append("&o");
-                    break;
-                default:
-                    break;
+    }
+    @Nullable
+    public String processPlaceholders(Player player) { //processes display name placeholder for message sent to player that had display name changed
+        String msg = config.getString("name-set");
+        if (msg != null) {
+            if (msg.contains("%display-name%")) {
+                msg = msg.replace("%display-name%", player.getDisplayName());
+            }
+            return ChatColor.translateAlternateColorCodes('&', msg);
+        }
+        return null;
+    }
+    @Nullable
+    public String processSenderPlaceholders(Player player){ //processes placeholders for sender
+        String msg = config.getString("name-set-other");
+        if(msg != null){
+            if(msg.contains("%display-name%")){
+                if(this.getMode().equals("essentials")){
+                    User user = essentials.getUser(player.getUniqueId());
+                    msg = msg.replace("%display-name%", user.getDisplayName());
+                }else{
+                    msg = msg.replace("%display-name%", player.getDisplayName());
                 }
             }
-        color.append(selectedPlayer.getName());
-        SetName.setNick(color.toString(), selectedPlayer, true);
-        if(!sender.equals(selectedPlayer)){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + NameColor.getInstance().processSenderPlaceholders(selectedPlayer));
+            if(msg.contains("%username%")){
+                if(this.getMode().equals("essentials")){
+                    User user = essentials.getUser(player.getUniqueId());
+                    msg = msg.replace("%username%", user.getDisplayName());
+                }else{
+                    msg = msg.replace("%username%", player.getName());
+                }
+            }
+            return ChatColor.translateAlternateColorCodes('&', msg);
         }
-        selectedPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + NameColor.getInstance().processPlaceholders(selectedPlayer));
-        return true;
+        return null;
+    }
+    public boolean notifyChange(){
+        return notify;
+    }
+    public String getDefaultColor(){
+        return defaultColor;
     }
 }
