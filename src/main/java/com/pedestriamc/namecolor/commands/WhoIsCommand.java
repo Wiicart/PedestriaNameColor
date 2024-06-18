79b8830f69b53f34487c1786a8fa1d1736ebb1a3
wiicart <sunglassesaj@gmail.com>
2024-06-18 18:49:03 -0500

    Update WhoIsCommand.java
    Add permission check

--- src/main/java/com/pedestriamc/namecolor/commands/WhoIsCommand.java
+++ src/main/java/com/pedestriamc/namecolor/commands/WhoIsCommand.java
@@ -1,4 +1,4 @@
-age com.pedestriamc.namecolor.commands;
+package com.pedestriamc.namecolor.commands;
 
 import com.pedestriamc.namecolor.NameColor;
 import com.pedestriamc.namecolor.SetName;
@@ -15,26 +15,31 @@ public class WhoIsCommand implements CommandExecutor {
     private final String whoIsMessage = config.getString("whois-message");
 
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, String label, String[] args){
-        String returnMessage = whoIsMessage;
-        //Check for insufficient args
-        if(args.length == 0){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("insufficient-args")));
-            return true;
-        }
-        //Check for too many args
-        if(args.length > 1){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-args-whois")));
-            return true;
-        }
-        //Check if can't find display name
-        if(SetName.getPlayer(args[0]) == null){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-player")));
-            return true;
+        if(sender.hasPermission("namecolor.whois") || sender.hasPermission("namecolor.*")){
+            String returnMessage = whoIsMessage;
+            //Check for insufficient args
+            if(args.length == 0){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("insufficient-args")));
+                return true;
+            }
+            //Check for too many args
+            if(args.length > 1){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-args-whois")));
+                return true;
+            }
+            //Check if can't find display name
+            if(SetName.getPlayer(args[0]) == null){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-player")));
+                return true;
+            }else{
+                //All good, sending who is msg
+                returnMessage = returnMessage.replace("%display-name%", args[0]);
+                returnMessage = returnMessage.replace("%username%", SetName.getPlayer(args[0]));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&',returnMessage));
+                return true;
+            }
         }else{
-            //All good, sending who is msg
-            returnMessage = returnMessage.replace("%display-name%", args[0]);
-            returnMessage = returnMessage.replace("%username%", SetName.getPlayer(args[0]));
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&',returnMessage));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("no-perms")));
             return true;
         }
     }
