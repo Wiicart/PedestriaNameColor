23eee9f5b5d9237c435c4cffe4cb7d0be59bcb0c
wiicart <sunglassesaj@gmail.com>
2024-06-20 15:56:21 -0500

    Update JoinListener.java

--- src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
+++ src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
@@ -1,5 +1,9 @@
-package com.pedestriamc.namecolor;
+package com.pedestriamc.namecolor.listeners;
 
+import com.pedestriamc.namecolor.NameColor;
+import com.pedestriamc.namecolor.SetName;
+import com.pedestriamc.namecolor.StoredPlayer;
+import com.pedestriamc.namecolor.StoredPlayers;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -21,9 +25,11 @@ public class JoinListener implements Listener {
                     SetName.setColor(event.getPlayer(), player.getChatColor(), false);
                     break;
             }
+            SetName.addPlayer(event.getPlayer());
         }else{
             //Update display name to default color code from config
             SetName.setColor(event.getPlayer(), NameColor.getInstance().getDefaultColor(), false);
+            SetName.addPlayer(event.getPlayer());
         }
     }
 }
