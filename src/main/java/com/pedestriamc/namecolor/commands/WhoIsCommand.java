4b7c793f7959ffdd4e5219de660b7b19078c6ed6
wiicart <sunglassesaj@gmail.com>
2024-07-03 13:50:34 -0500

    Update WhoIsCommand.java

--- src/main/java/com/pedestriamc/namecolor/commands/WhoIsCommand.java
+++ src/main/java/com/pedestriamc/namecolor/commands/WhoIsCommand.java
@@ -1,45 +1,42 @@
-package com.pedestriamc.namecolor.commands;
+age com.pedestriamc.namecolor.commands;
 
-import com.pedestriamc.namecolor.NameColor;
+import com.pedestriamc.namecolor.Messenger;
 import com.pedestriamc.namecolor.SetName;
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 public class WhoIsCommand implements CommandExecutor {
 
-    private final FileConfiguration config = NameColor.getInstance().getConfigFile();
-    private final String whoIsMessage = config.getString("whois-message");
-
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, String label, String[] args){
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args){
         if(sender.hasPermission("namecolor.whois") || sender.hasPermission("namecolor.*")){
-            String returnMessage = whoIsMessage;
             //Check for insufficient args
             if(args.length == 0){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("insufficient-args")));
+                Messenger.sendMessage(sender, Messenger.Message.INSUFFICIENT_ARGS);
                 return true;
             }
             //Check for too many args
             if(args.length > 1){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-args-whois")));
+                Messenger.sendMessage(sender, Messenger.Message.INVALID_ARGS_WHOIS);
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("color")){
+                Messenger.sendMessage(sender, Messenger.Message.COLORS);
                 return true;
             }
             //Check if can't find display name
             if(SetName.getPlayer(args[0]) == null){
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("invalid-player")));
+                Messenger.sendMessage(sender, Messenger.Message.INVALID_PLAYER);
                 return true;
             }else{
                 //All good, sending who is msg
-                returnMessage = returnMessage.replace("%display-name%", args[0]);
-                returnMessage = returnMessage.replace("%username%", SetName.getPlayer(args[0]));
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&',returnMessage));
+                Messenger.processPlaceholders(sender, Messenger.Message.WHOIS_MESSAGE, Bukkit.getPlayer(SetName.getPlayer(args[0])));
                 return true;
             }
         }else{
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', NameColor.getInstance().getPrefix()) + ChatColor.translateAlternateColorCodes('&', config.getString("no-perms")));
+            Messenger.sendMessage(sender, Messenger.Message.NO_PERMS);
             return true;
         }
     }
