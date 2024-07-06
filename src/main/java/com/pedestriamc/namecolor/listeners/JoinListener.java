851537cbeb2aed3817755f2d5de62b0ade7fee4e
wiicart <sunglassesaj@gmail.com>
2024-07-06 13:26:32 -0500

    Update JoinListener.java

--- src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
+++ src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
@@ -1,7 +1,7 @@
 package com.pedestriamc.namecolor.listeners;
 
 import com.pedestriamc.namecolor.NameColor;
-import com.pedestriamc.namecolor.SetName;
+import com.pedestriamc.namecolor.NameUtilities;
 import com.pedestriamc.namecolor.StoredPlayer;
 import com.pedestriamc.namecolor.StoredPlayers;
 import org.bukkit.event.EventHandler;
@@ -14,22 +14,14 @@ public class JoinListener implements Listener {
         StoredPlayer player = StoredPlayers.loadStoredPlayer(event.getPlayer());
         if(player != null){
             // Determines the way the name is set, and uses the proper method to set displayName
-            switch (player.getType()){
-                case NICKNAME:
-                    SetName.setNick(player.getNickname(), event.getPlayer(), false);
-                    break;
-                case RGB_COLOR:
-                    SetName.setColor(event.getPlayer(), player.getColor(), false);
-                    break;
-                case CHAT_COLOR:
-                    SetName.setColor(event.getPlayer(), player.getChatColor(), false);
-                    break;
+            switch (player.getType()) {
+                case NICKNAME -> NameUtilities.setNick(player.getNickname(), event.getPlayer(), false);
+                case RGB_COLOR -> NameUtilities.setColor(event.getPlayer(), player.getColor(), false);
+                case CHAT_COLOR -> NameUtilities.setColor(event.getPlayer(), player.getChatColor(), false);
             }
-            SetName.addPlayer(event.getPlayer());
         }else{
             //Update display name to default color code from config
-            SetName.setColor(event.getPlayer(), NameColor.getInstance().getDefaultColor(), false);
-            SetName.addPlayer(event.getPlayer());
+            NameUtilities.setColor(event.getPlayer(), NameColor.getInstance().getDefaultColor(), false);
         }
     }
 }
