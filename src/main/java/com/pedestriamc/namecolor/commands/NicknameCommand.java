acf1db3d799ad8ddb6218aaf6af10902ccb71b24
wiicart <sunglassesaj@gmail.com>
2024-07-06 14:02:09 -0500

    Update NicknameCommand.java

--- src/main/java/com/pedestriamc/namecolor/commands/NicknameCommand.java
+++ src/main/java/com/pedestriamc/namecolor/commands/NicknameCommand.java
@@ -1,8 +1,9 @@
 package com.pedestriamc.namecolor.commands;
 
+import com.pedestriamc.namecolor.Message;
 import com.pedestriamc.namecolor.Messenger;
 import com.pedestriamc.namecolor.NameColor;
-import com.pedestriamc.namecolor.SetName;
+import com.pedestriamc.namecolor.NameUtilities;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -13,81 +14,92 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class NicknameCommand implements CommandExecutor {
+
+    private final boolean notifyChange = NameColor.getInstance().notifyChange();
+
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         Player selectedPlayer;
         String nick;
         //Check for enough args
         if(args.length == 0){
-            Messenger.sendMessage(sender, Messenger.Message.INSUFFICIENT_ARGS);
+            Messenger.sendMessage(sender, Message.INSUFFICIENT_ARGS);
             return true;
         }
         //Check for too many/invalid args
         if(args.length > 2){
-            Messenger.sendMessage(sender, Messenger.Message.INVALID_ARGS_NICK);
+            Messenger.sendMessage(sender, Message.INVALID_ARGS_NICK);
             return true;
         }
         //Help message
-        if (args[0].equalsIgnoreCase("HELP")) {
-            Messenger.sendMessage(sender, Messenger.Message.NICKNAME_HELP);
+        if (args[0].equalsIgnoreCase("help")) {
+            Messenger.sendMessage(sender, Message.NICKNAME_HELP);
             return true;
         }
         if(args.length == 1){
             if(!sender.hasPermission("namecolor.nick") && !sender.hasPermission("namecolor.*") && !sender.hasPermission("namecolor.nick.*")){
-                Messenger.sendMessage(sender, Messenger.Message.NO_PERMS);
+                Messenger.sendMessage(sender, Message.NO_PERMS);
                 return true;
             }
             if(sender instanceof Player){
-                if (args[0].equalsIgnoreCase("RESET")) {
-                    SetName.setNick(sender.getName(),(Player) sender,true);
-                    Messenger.processPlaceholders(sender, Messenger.Message.NAME_SET, (Player) sender);
+                if (args[0].equalsIgnoreCase("reset")) {
+                    NameUtilities.setNick(sender.getName(),(Player) sender,true);
+                    if(notifyChange){
+                        Messenger.processPlaceholders(sender, Message.NAME_SET, (Player) sender);
+                    }
                     return true;
                 }
                 selectedPlayer = (Player) sender;
                 nick = args[0];
                 if(ChatColor.stripColor(nick).length() > NameColor.getInstance().nickLengthLimit()){
-                    Messenger.sendMessage(sender, Messenger.Message.NICK_TOO_LONG);
+                    Messenger.sendMessage(sender, Message.NICK_TOO_LONG);
                     return true;
                 }
                 if(isDuplicateNickname(nick, (Player) sender)){
-                    Messenger.sendMessage(sender, Messenger.Message.USERNAME_NICK_PROHIBITED);
+                    Messenger.sendMessage(sender, Message.USERNAME_NICK_PROHIBITED);
                     return true;
                 }
-                SetName.setNick(nick, selectedPlayer,true);
-                Messenger.processPlaceholders(selectedPlayer, Messenger.Message.NAME_SET, selectedPlayer);
+                NameUtilities.setNick(nick, selectedPlayer,true);
+                if(notifyChange){
+                    Messenger.processPlaceholders(selectedPlayer, Message.NAME_SET, selectedPlayer);
+                }
             }else{
                 //THIS MEANS SERVER SENT COMMAND WITH ONE ARG, CONFIG W/ LANG
-                Messenger.sendMessage(sender, Messenger.Message.INSUFFICIENT_ARGS);
+                Messenger.sendMessage(sender, Message.INSUFFICIENT_ARGS);
             }
             return true;
         }
         //At this point, command has 2 args, meaning that it is setting another player's nickname
         if(!sender.hasPermission("namecolor.nick.others")){
-            Messenger.sendMessage(sender, Messenger.Message.NO_PERMS);
+            Messenger.sendMessage(sender, Message.NO_PERMS);
             return true;
         }
         selectedPlayer = Bukkit.getPlayer(args[1]);
         if(selectedPlayer != null){
             if (args[0].equalsIgnoreCase("RESET")) {
-                SetName.setNick(selectedPlayer.getName(), selectedPlayer,true);
-                Messenger.processPlaceholders(selectedPlayer, Messenger.Message.NAME_SET, (Player) sender);
-                Messenger.processPlaceholders(sender, Messenger.Message.NAME_SET_OTHER, (Player) sender);
+                NameUtilities.setNick(selectedPlayer.getName(), selectedPlayer,true);
+                if(notifyChange){
+                    Messenger.processPlaceholders(selectedPlayer, Message.NAME_SET, (Player) sender);
+                }
+                Messenger.processPlaceholders(sender, Message.NAME_SET_OTHER, (Player) sender);
                 return true;
             }
             if(ChatColor.stripColor(args[0]).length() > NameColor.getInstance().nickLengthLimit()){
-                Messenger.sendMessage(sender, Messenger.Message.NICK_TOO_LONG);
+                Messenger.sendMessage(sender, Message.NICK_TOO_LONG);
                 return true;
             }
             if(isDuplicateNickname(args[0], selectedPlayer)){
-                Messenger.sendMessage(sender, Messenger.Message.USERNAME_NICK_PROHIBITED);
+                Messenger.sendMessage(sender, Message.USERNAME_NICK_PROHIBITED);
                 return true;
             }
-            SetName.setNick(args[0], Bukkit.getPlayer(args[1]), true);
+            NameUtilities.setNick(args[0], Bukkit.getPlayer(args[1]), true);
             if(!sender.equals(selectedPlayer)){
-                Messenger.processPlaceholders(sender, Messenger.Message.NAME_SET_OTHER, selectedPlayer);
+                Messenger.processPlaceholders(sender, Message.NAME_SET_OTHER, selectedPlayer);
             }
-            Messenger.processPlaceholders(selectedPlayer, Messenger.Message.NAME_SET, selectedPlayer);
+            if(notifyChange){
+                Messenger.processPlaceholders(selectedPlayer, Message.NAME_SET, selectedPlayer);
+            }
         }else{
-            Messenger.sendMessage(sender, Messenger.Message.INVALID_PLAYER);
+            Messenger.sendMessage(sender, Message.INVALID_PLAYER);
         }
         return true;
     }
