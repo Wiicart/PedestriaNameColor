9de4901fe18a65a74efb7c3fa2d68ecaf0964f3b
wiicart <sunglassesaj@gmail.com>
2024-07-22 14:07:51 -0500

    Update LeaveListener.java

--- src/main/java/com/pedestriamc/namecolor/listeners/LeaveListener.java
+++ src/main/java/com/pedestriamc/namecolor/listeners/LeaveListener.java
@@ -1,6 +1,6 @@
 package com.pedestriamc.namecolor.listeners;
 
-import com.pedestriamc.namecolor.SetName;
+import com.pedestriamc.namecolor.NameUtilities;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -8,7 +8,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class LeaveListener implements Listener {
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event){
-        SetName.removePlayer(event.getPlayer());
+        NameUtilities.removePlayer(event.getPlayer());
+        NameUtilities.removeTeam(event.getPlayer());
     }
-
 }
